b26cda35cd5fc405defc75275d40c1ba43f256c5
elazar matan <elazarmata8@gmail.com>
2025-05-22 11:14:50 +0300

    the class AttackTools

---
+++ AttakTools.cs
@@ -0,0 +1,15 @@
+﻿public abstract class AttakTools
+{
+    protected string booms;
+    protected string efective;
+    protected int strikes;
+
+    public AttakTools(string booms, string efective, int strikes)
+    {
+        this.booms = booms;
+        this.efective = efective;
+        this.strikes = strikes;
+    }
+
+    public abstract void setStrike(int newStrike);
+}
