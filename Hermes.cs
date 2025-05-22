61573afe744647b406980fc9db30a71f53c8da61
elazar matan <elazarmata8@gmail.com>
2025-05-22 11:15:55 +0300

    the class hermes

---
+++ Hermes.cs
@@ -0,0 +1,11 @@
+﻿public class Hermes460 : AttakTools
+{
+    public Hermes460() : base("personnel or armored vehicles", "pepole , vechiles", 3)
+    {
+    }
+    public override void setStrike(int newStrike)
+    {
+        strikes -= newStrike;
+    }
+
+}
