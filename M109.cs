b245626096b6919dd7f476081e0c81e944da6a6a
elazar matan <elazarmata8@gmail.com>
2025-05-22 11:13:49 +0300

    the class M109

---
+++ M109.cs
@@ -0,0 +1,11 @@
+﻿public class M109 : AttakTools
+{
+    public M109 () : base("explosive shells", "open areas", 40)
+    {
+    }
+
+    public override void setStrike(int newStrike)
+    {
+        strikes -= newStrike;
+    }
+}
