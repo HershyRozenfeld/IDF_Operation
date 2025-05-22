48d3c77e5efa084d107a51404dbdb0e1f34744f9
elazar matan <elazarmata8@gmail.com>
2025-05-22 16:24:52 +0300

    the class terorist

--- Terrorist.cs
+++ Terrorist.cs
@@ -1,4 +1,39 @@
-﻿public class Terrorist : Hamas
+﻿public class Terrorist
 {
+    private  string name;
+    protected  string weapon;
+    protected  int rank;
+    protected bool status = true;
 
+    public Terrorist(string name,string weapon,int rank)
+    {
+        this.name = name;
+        this.weapon = weapon;
+        this.rank = rank;
+    }
+
+    public void dead()
+    {
+        this.status = false;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public string getWeapon()
+    {
+        return weapon;
+    }
+
+    public int getRank()
+    {
+        return rank;
+    }
+
+    public bool getStatus()
+    {
+        return status;
+    }
 }
