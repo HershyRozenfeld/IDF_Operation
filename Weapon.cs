638e2379b50d12cf5b91f9aa253c624244a2f0c3
hershy rozenfeld <Y6764057@gmail.com>
2025-06-04 21:46:56 +0300

    Refactor(Weapon): Decouple console output from ToString method

--- Weapon.cs
+++ Weapon.cs
@@ -6,7 +6,7 @@ public abstract class Weapon
     public string booms;
     public string effective;
     public int strikes;
-    public int id;
+    public int id; // not initialized or used in the constructor or methods.
 
     public Weapon(string name, string booms, string effective, int strikes)
     {
@@ -15,17 +15,13 @@ public abstract class Weapon
         this.effective = effective;
         this.strikes = strikes;
     }
-
+    public abstract void setStrike(int fgh);
     public override string ToString()
     {
-        Console.ForegroundColor = ConsoleColor.Cyan;
         string output = $"│ Weapon: {name}\n";
         output += $"│ - Boom Type: {booms}\n";
         output += $"│ - Effective Area: {effective}\n";
         output += $"│ - Strikes Left: {strikes}\n";
-        Console.ResetColor();
         return output;
     }
-
-    public abstract void setStrike(int fgh);
 }
